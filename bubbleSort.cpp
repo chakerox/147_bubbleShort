96bcc0b345edb55cae76690491945ffceae87e49
chakerox <chakerox@gmail.com>
2026-03-05 11:31:16 +0700

    membuat prosedur untuk

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -26,3 +26,28 @@ void input() { // prosedur untuk input
         cin >> arr[i];                         // Input dari pengguna
     }
 }
+
+void bubbleSortArray()  {  // procedur untuk mengurutkan array dengan
+    int pass = 1; // step 1
+
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+            if (arr[j] > arr[j + 1]) { // step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1; //step 4
+
+        cout << "\nPass " << pass - 1 << ": ";// Number of pass
+        for (int k = 0; k < n; k++)  {
+            cout << arr[k] << " ";         // Menampilkan data pada number of pass
+
+        }
+        cout << endl;
+
+    } while (pass <= n - 1); // step 5
+}
